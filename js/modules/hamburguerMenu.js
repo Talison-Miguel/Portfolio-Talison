762901321bdf25bb8a9f187a2b070bc60e690f1a
Tálison Miguel <talisonmiguel84@gmail.com>
2023-12-28 13:46:38 -0300

    fix: hamburguerMenu

--- js/modules/hamburguerMenu.js
+++ js/modules/hamburguerMenu.js
@@ -2,21 +2,20 @@ let menuHamburguer   = document.querySelector('#checkbox-menu');
 let menuList = document.querySelector('.header-top-navigation')
 let menuListlinks = [...document.querySelectorAll('.header-top-navigation li a')]
 const links = [...menuList.querySelectorAll('li')]
-
+let timerId;
 
 function openHamburgerMenu() {
     if(menuHamburguer.checked === true) {
 
         menuList.classList.add('menuHamburguer')
-        setTimeout(() => menuList.style.transform = 'translateY(0)' ,40)
-
-        links.map(link => {
-            setTimeout(() => link.style.opacity = 1, 120)
-        })
+        clearTimeout(timerId);
+        timerId = setTimeout(() => menuList.style.transform = 'translateY(0)' ,40)
+        links.map(link => setTimeout(() => link.style.opacity = 1, 120))
     } else if(menuHamburguer.checked === false) {
 
         menuList.style.transform = 'translateY(-140%)'  
-        setTimeout(() => menuList.classList.remove('menuHamburguer'), 500)
+        clearTimeout(timerId);
+        timerId = setTimeout(() => menuList.classList.remove('menuHamburguer'), 500)
         links.map(link => link.style.opacity = 0) 
     }
 } ;
@@ -24,13 +23,15 @@ function openHamburgerMenu() {
 
 function handleResize() {
     if(window.innerWidth >= 1090) {
+
         setTimeout(() => menuList.style.transform = 'translateY(0)' , 20)
         links.map(link => link.style.opacity = 1)  
         menuList.classList.remove('menuHamburguer')
         menuHamburguer.checked = false
     } 
-}
 
+    if(window.innerWidth < 1090) menuList.style.transform = 'translateY(-140%)'
+}
 handleResize();
 
 function closeHamburgerMenu() {
@@ -41,7 +42,5 @@ function closeHamburgerMenu() {
         menuHamburguer.checked = false
     }
 }
-
-console.log(menuListlinks)
 
 export {openHamburgerMenu, menuHamburguer, handleResize, closeHamburgerMenu, menuListlinks}
