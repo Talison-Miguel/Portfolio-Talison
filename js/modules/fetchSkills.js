82c92a3fe3e2cb1c596a64d0e50b30f9456f38e9
Tálison Miguel <talisonmiguel84@gmail.com>
2024-01-13 17:24:56 -0300

    fix: bug

--- js/modules/fetchSkills.js
+++ js/modules/fetchSkills.js
@@ -25,8 +25,13 @@ export async function getData(data) {
             svg.style.transition = 'fill .3s ease-in'
             svg.style.cursor = 'pointer'
             
-            elementoPai.addEventListener('mouseover', () => {svg.style.fill = 'white'});
-            elementoPai.addEventListener('mouseout', () => {svg.style.fill = '#00b8e4'});
+            elementoPai.addEventListener('mouseover', () => {
+                svg.style.fill = 'white'
+            });
+            
+            elementoPai.addEventListener('mouseout', () => {
+                svg.style.fill = '#00b8e4'
+            });
 
         })
     });
